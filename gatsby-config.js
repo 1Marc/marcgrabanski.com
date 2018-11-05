db7011afa1acdb56dcb1b9fd687644ac9a6e4cf9
Marc Grabanski <marc@mjg.in>
2018-11-04 23:35:16 -0600

    Update GA ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-963303-2' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
