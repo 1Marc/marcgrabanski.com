dba2491bc8c7fbcc6a2ee36a2112a9530c8ca580
Marc Grabanski <marc@mjg.in>
2020-01-01 11:54:49 -0600

    Fix tag links frontmatter

--- gatsby/on-create-node.js
+++ gatsby/on-create-node.js
@@ -25,7 +25,7 @@ const onCreateNode = ({ node, actions, getNode }) => {
 
     if (node.frontmatter.tags) {
       const tagSlugs = node.frontmatter.tags.map(
-        tag => `/tag/${_.kebabCase(tag)}/`
+        tag => `/tags/${_.kebabCase(tag)}/`
       )
       createNodeField({ node, name: 'tagSlugs', value: tagSlugs })
     }
