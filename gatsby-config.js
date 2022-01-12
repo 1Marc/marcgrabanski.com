2152cb4153942cfc769bdc501a8740d8a871e09e
Marc Grabanski <m@marcgrabanski.com>
2022-01-12 10:46:06 -0600

    Add title to feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,7 +96,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Your Site's RSS Feed",
+            title: "MarcGrabanski.com: Web Development & Business RSS Feed",
             // optional configuration to insert feed reference in pages:
             // if `string` is used, it will be used to create RegExp and then test if pathname of
             // current page satisfied this regular expression;
