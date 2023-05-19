d9d098ff2c61ebd857f11618b9f69fd211bfd4a5
Thomas Hayner <thom.hayner@gmail.com>
2023-05-18 22:58:26 -0600

    fix article ordering

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,7 +7,8 @@ export async function get(context) {
   const allArticles = await getCollection('articles', (article) => {
     return !article.data.isDraft && !article.data.isArchived && article.data.date <= new Date();
   });
-  console.log(allArticles.slice(0,1))
+  allArticles.reverse();
+
   return rss({
     title: site.url,
     description: site.title,
@@ -18,6 +19,7 @@ export async function get(context) {
       pubDate: article.data.date,
       description: article.data.description,
       link: `/articles/${article.slug}`,
+      content: article.body,
     })),
   });
 }
