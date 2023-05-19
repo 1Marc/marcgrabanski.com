a2dcc30fe0433a21b55d3b6665187748e8989cb4
Thomas Hayner <thom.hayner@gmail.com>
2023-05-18 22:34:54 -0600

    configure basic rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  site: 'https://www.marcgrabanski.com'
 });
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+// // https://docs.astro.build/en/guides/rss/
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { site } from '../data/siteMetaData';
+
+export async function get(context) {
+  const allArticles = await getCollection('articles', (article) => {
+    return !article.data.isDraft && !article.data.isArchived && article.data.date <= new Date();
+  });
+  console.log(allArticles.slice(0,1))
+  return rss({
+    title: site.url,
+    description: site.title,
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site,
+    items: allArticles.map((article) => ({
+      title: article.data.postTitle,
+      pubDate: article.data.date,
+      description: article.data.description,
+      link: `/articles/${article.slug}`,
+    })),
+  });
+}
