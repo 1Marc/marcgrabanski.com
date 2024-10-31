7b0da8ada04723bb774dbd66a77fc62ad6defa0a
Marc Grabanski <m@marcgrabanski.com>
2024-10-31 11:29:31 -0500

    trying crap from the docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://www.marcgrabanski.com/'
+  site: "https://www.marcgrabanski.com/",
+  build: {
+    rollupOptions: {
+      external: ["sharp"],
+    },
+  },
+  image: {
+    // Example: Enable the Sharp-based image service with a custom config
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        limitInputPixels: false,
+      },
+    },
+  },
 });
