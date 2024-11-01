0e5ab6c2b58ff67a8d1111d3c001ebcbfb864422
Marc Grabanski <m@marcgrabanski.com>
2024-11-01 12:22:37 -0500

    Prettier all the things

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const articles = defineCollection({
   schema: z.object({
     postTitle: z.string(),
     description: z.string(),
     tags: z.array(z.string()),
-    date: z.string().transform(str => new Date(str)),
+    date: z.string().transform((str) => new Date(str)),
     isDraft: z.boolean(),
     isArchived: z.boolean(),
     layout: z.string(),
